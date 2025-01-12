ca30680a6156d06066c5def35c57affc0a249ac5
unknown <johnashimedua@outlook.com>
2025-01-12 16:46:06 +0000

    testing repo name change

--- backend/settings.py
+++ backend/settings.py
@@ -207,6 +207,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_URL = '/static/'
 
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
