d70a4fb5090d5ca54f5d7d75f24d8065314a3f0a
unknown <johnashimedua@outlook.com>
2025-01-13 15:22:00 +0000

    cronjob for monthly balance done

--- backend/settings.py
+++ backend/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     "corsheaders",
     "drf_spectacular",
     "drf_spectacular_sidecar",
-    "django_crontab",
     "auth_custom",
     "members",
     "admin_custom",
@@ -109,10 +108,6 @@ CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS").split(",")
 
-
-CRONJOBS = [
-    ('* * * * *', 'finance.cron.monthly_balance_update')
-]
 
 ROOT_URLCONF = "backend.urls"
 
--- seeders/management/commands/monthly_balance_job.py
+++ seeders/management/commands/monthly_balance_job.py
@@ -2,8 +2,6 @@ from finance.models import Account, Monthly_Balance
 from django.utils import timezone
 from backend.utils import time_date
 from django.core.management.base import BaseCommand, CommandError
-
-
 import logging
 
 logger = logging.getLogger(__name__)
@@ -14,9 +12,32 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         today = timezone.now()
-        logger.info(f"Mise a jour du Solde Du Mois de {time_date.list_month[today.month-1]}")
-
-
-        self.stdout.write(
-            self.style.SUCCESS('Job complete')
+        logger.info(
+            f"Mise a jour du Solde Du Mois de {time_date.list_month[today.month-1]}"
         )
+        try:
+            month = today.month
+            year = today.year
+            all_accounts = Account.objects.all()
+            for acc in all_accounts:
+                present_account_balance = Monthly_Balance.objects.filter(
+                    month=month, year=year, account=acc
+                )
+
+                if present_account_balance.exists():
+                    present_account_balance.first().balance = acc.balance
+                    logger.info(f"Updating {present_account_balance.first()}")
+                else:
+                    Monthly_Balance.objects.create(
+                        month=month, year=year, account=acc, balance=acc.balance
+                    )
+                    logger.info(
+                        f"Inserting New Record for {present_account_balance.first()}"
+                    )
+
+            logger.info(f"Monthly Balance Job done")
+            self.stdout.write(self.style.SUCCESS("Job completed"))
+        except Exception as m:
+            logger.exception(m)
+            self.stdout.write(self.style.ERROR("Job Failed"))
+
